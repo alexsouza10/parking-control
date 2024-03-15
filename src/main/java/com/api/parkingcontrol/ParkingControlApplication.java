a243b57fee215b352be118808a47b8bda374e501
alexsouza10 <alexsouza160298@gmail.com>
2024-03-15 18:31:14 -0300

    Alteration in ParkingControlApplication

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -6,14 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ParkingControlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ParkingControlApplication.class, args);
-	}
-	@GetMapping("/")
-	public String index(){
-		return "Ola Mundo!";
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingControlApplication.class, args);
+    }
+
+    @RestController
+    public static class IndexController {
+        @GetMapping("/")
+        public String index(){
+            return "Ola Mundo!";
+        }
+    }
 }
